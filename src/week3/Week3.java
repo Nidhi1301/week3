374de495140c9f420c15aafc605e50bb33c42d98
Nidhi1301 <nidhinid@sheridancollege.ca>
2020-01-23 09:32:31 -0500

    This is my secong commit with println

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -15,7 +15,7 @@ public class Week3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("This is modification of print statement");
     }
     
 }
